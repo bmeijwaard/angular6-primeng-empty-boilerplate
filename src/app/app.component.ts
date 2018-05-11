768a73ce18e16dbadb2fed91ffa4d36d723aaedb
Bob Meijwaard <bmeijwaard@gmail.com>
2018-05-11 09:11:15 +0200

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
+import { MenuModule } from 'primeng/menu';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  items: MenuItem[];
+  title = 'Hello world!';
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'File',
+        items: [
+          {
+            label: 'New',
+            icon: 'fa-plus',
+            items: [{ label: 'Project' }, { label: 'Other' }]
+          },
+          { label: 'Open' },
+          { label: 'Quit' }
+        ]
+      },
+      {
+        label: 'Edit',
+        icon: 'fa-edit',
+        items: [
+          { label: 'Undo', icon: 'fa-mail-forward' },
+          { label: 'Redo', icon: 'fa-mail-reply' }
+        ]
+      }
+    ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenubarModule } from 'primeng/menubar';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MenubarModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
